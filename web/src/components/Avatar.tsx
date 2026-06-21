b766e7e4920eb6c6230e5f7b3f0bc393547d9bd3
Vladislav Makashov <makashov73@gmail.com>
2026-06-22 02:51:41 +0400

    feat(web): Avatar draws deceased X overlay when marked

--- web/src/components/Avatar.tsx
+++ web/src/components/Avatar.tsx
@@ -1,34 +1,46 @@
 import type { Gender } from "../types.js";
 import { avatarKey } from "../lib/avatar.js";
-import { avatarSvgMarkup } from "../lib/avatarSvg.js";
+import { avatarSvgMarkup, deceasedOverlaySvg } from "../lib/avatarSvg.js";
 
 interface Props {
   gender: Gender;
   age?: number | null;
   size?: number;
   src?: string | null;
+  deceased?: boolean;
 }
 
-export function Avatar({ gender, age, size = 56, src }: Props) {
-  if (src) {
-    return (
-      <img
-        data-testid="avatar-img"
-        src={src}
-        alt=""
-        width={size}
-        height={size}
-        style={{ width: size, height: size, borderRadius: "50%", objectFit: "cover", display: "inline-block" }}
-      />
-    );
-  }
-  return (
+export function Avatar({ gender, age, size = 56, src, deceased }: Props) {
+  const inner = src ? (
+    <img
+      data-testid="avatar-img"
+      src={src}
+      alt=""
+      width={size}
+      height={size}
+      style={{ width: size, height: size, borderRadius: "50%", objectFit: "cover", display: "block" }}
+    />
+  ) : (
     <span
       data-testid="avatar"
       data-avatar={avatarKey(gender, age)}
       aria-label={avatarKey(gender, age)}
-      style={{ display: "inline-block", width: size, height: size, lineHeight: 0 }}
+      style={{ display: "block", width: size, height: size, lineHeight: 0 }}
       dangerouslySetInnerHTML={{ __html: avatarSvgMarkup(gender, age) }}
     />
+  );
+
+  return (
+    <span style={{ position: "relative", display: "inline-block", width: size, height: size, lineHeight: 0 }}>
+      {inner}
+      {deceased && (
+        <span
+          data-testid="avatar-deceased"
+          aria-hidden="true"
+          style={{ position: "absolute", inset: 0, lineHeight: 0 }}
+          dangerouslySetInnerHTML={{ __html: deceasedOverlaySvg() }}
+        />
+      )}
+    </span>
   );
 }
